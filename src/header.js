4513ec1d87f25bac20d789d27fdb0d1f6d43f478
Richard <richardto180@gmail.com>
2024-01-12 16:26:04 -0500

    Appended elements

--- src/header.js
+++ src/header.js
@@ -1,11 +1,12 @@
-import { createHtml } from "./createDOM";
+import { createHtml, container } from "./createDOM";
 
 const render = () => {
+  // const header = document.createElement('div');
   const header = createHtml(
     'div',
     null,
     ['header'],
-    null,
+    '',
   );
 
   const headerTitle = createHtml(
@@ -14,6 +15,9 @@ const render = () => {
     ['header-title'],
     'To-Do List'
   );
+
+  container.appendChild(header);
+  header.appendChild(headerTitle);
 }
 
 export { render as renderHeader };
